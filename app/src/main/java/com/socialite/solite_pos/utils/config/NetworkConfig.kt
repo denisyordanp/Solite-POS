160b77b9ea563ec622a48fa200bea3930afd1d43
Denis Y Panggabean <denis.yordan.p@gmail.com>
2023-06-16 19:42:25 +0700

    add timeout

--- app/src/main/java/com/socialite/solite_pos/utils/config/NetworkConfig.kt
+++ app/src/main/java/com/socialite/solite_pos/utils/config/NetworkConfig.kt
@@ -7,4 +7,6 @@ object NetworkConfig {
     fun isDebugMode(): Boolean = BuildConfig.DEBUG
 
     fun getBaseUrl(): String = "https://api-solite-release.denisyordanp.com/"
+
+    fun timeout(): Long = 60
 }
