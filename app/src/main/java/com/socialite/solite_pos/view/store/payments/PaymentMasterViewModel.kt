d81d36d94c8af23b44249855c1096cb5b43d0a92
Denis Y Panggabean <denis.yordan.p@gmail.com>
2023-06-22 07:52:34 +0700

    complete payment screen

--- app/src/main/java/com/socialite/solite_pos/view/store/payments/PaymentMasterViewModel.kt
+++ app/src/main/java/com/socialite/solite_pos/view/store/payments/PaymentMasterViewModel.kt
@@ -1,16 +1,16 @@
 package com.socialite.solite_pos.view.store.payments
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.sqlite.db.SupportSQLiteQuery
 import com.socialite.solite_pos.data.source.local.entity.room.new_master.Payment
 import com.socialite.solite_pos.data.source.repository.PaymentsRepository
-import com.socialite.solite_pos.di.loggedin.LoggedInRepositoryInjection
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PaymentMasterViewModel(
+@HiltViewModel
+class PaymentMasterViewModel @Inject constructor(
     private val paymentRepository: PaymentsRepository,
 ) : ViewModel() {
 
@@ -25,17 +25,6 @@ class PaymentMasterViewModel(
     fun updatePayment(data: Payment) {
         viewModelScope.launch {
             paymentRepository.updatePayment(data)
-        }
-    }
-
-    companion object {
-        fun getFactory(context: Context) = object : ViewModelProvider.Factory {
-            @Suppress("UNCHECKED_CAST")
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return PaymentMasterViewModel(
-                    paymentRepository = LoggedInRepositoryInjection.providePaymentsRepository(context)
-                ) as T
-            }
         }
     }
 }
