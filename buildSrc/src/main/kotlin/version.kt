47217d7217cc2d8b3edb257fb79da90a81785788
Denis Y Panggabean <denis.yordan.p@gmail.com>
2023-07-26 17:13:45 +0700

    build success

---
+++ buildSrc/src/main/kotlin/version.kt
@@ -0,0 +1,53 @@
+// Kotlin
+const val kotlinVersion = "1.7.10"
+const val kotlinCoreVersion = "1.9.0"
+const val kotlinCoroutineVersion = "1.6.4"
+
+val gradleVersion = "7.3.0"
+
+//Google
+val googleServiceVersion = "4.3.8"
+val firebaseBomVersion = "26.5.0"
+val crashlyticsVersion = "2.9.2"
+val googlePlayVersion = "2.1.0"
+
+// Compose
+val composeBomVersion = "2022.10.00"
+val composeVersion = "1.2.1"
+val activityComposeVersion = "1.6.0"
+val accompanistPagerVersion = "0.23.1"
+val constraintComposeVersion = "1.0.1"
+val navigationComposeVersion = "2.5.2"
+
+// Hilt
+val hiltVersion = "2.44"
+val hiltNavigationComposeVersion = "1.0.0"
+
+// Database
+val roomVersion = "2.4.3"
+val sqliteJdbc = "3.36.0.1"
+val dataStoreVersion = "1.0.0"
+
+// Lifecycle
+val lifecycleVersion = "2.5.1"
+val lifecycleExtensionVersion = "2.2.0"
+val appCompatVersion = "1.5.1"
+
+// UI
+val materialVersion = "1.6.1"
+val constraintLayoutVersion = "2.0.4"
+val recycleViewVersion = "1.1.0"
+val cardViewVersion = "1.0.0"
+
+// Utility
+val easyPermissionVersion = "3.0.0"
+val securityCryptoVersion = "1.1.0-alpha03"
+
+// Network
+val retrofitVersion = "2.9.0"
+val okhttpLoggingVersion = "4.9.3"
+
+// Testing
+val junitVersion = "4.13.2"
+val junitExtVersion = "1.1.5"
+val espressoVersion = "3.5.1"
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+
+        maven {url = uri("https://plugins.gradle.org/m2/")}
+    }
+}
+
+rootProject.name = "Solite-POS"
+include("app")
