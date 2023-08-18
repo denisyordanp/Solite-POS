7cdfcfa41b47b9e8dd9aceeab0787c56ffa144c4
denisyordanp <denis.yordan.p@gmail.com>
2023-08-18 09:07:00 +0700

    implemented the modularization phase 4

--- app/src/main/java/com/socialite/solite_pos/data/schema/response/ResponseData.kt
+++
@@ -1,8 +0,0 @@
-package com.socialite.solite_pos.data.schema.response
-
-import com.socialite.solite_pos.data.schema.room.EntityData
-
-interface ResponseData {
-    val id: String
-    fun toEntity(): EntityData
-}
--- app/src/main/java/com/socialite/solite_pos/data/schema/response/TokenResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.socialite.solite_pos.data.schema.response
-
-data class TokenResponse(
-    val token: String
-)
