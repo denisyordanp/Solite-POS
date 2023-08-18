ba69da90dba28262976a21e0a8ea2a207775f126
denisyordanp <denis.yordan.p@gmail.com>
2023-08-18 09:03:14 +0700

    implemented the modularization phase 3

---
+++ app/src/main/java/com/socialite/solite_pos/data/schema/Payment.kt
@@ -0,0 +1,29 @@
+package com.socialite.solite_pos.data.schema
+
+import com.socialite.solite_pos.view.ui.DropdownItem
+
+import com.socialite.data.schema.room.new_master.Payment as DataPayment
+
+data class Payment(
+    val id: String,
+    override val name: String,
+    val desc: String,
+    val tax: Float,
+    val isCash: Boolean,
+    val isActive: Boolean,
+    val isUploaded: Boolean
+) : DropdownItem {
+    companion object {
+        fun fromData(data: DataPayment): Payment {
+            return Payment(
+                data.id,
+                data.name,
+                data.desc,
+                data.tax,
+                data.isCash,
+                data.isActive,
+                data.isUploaded
+            )
+        }
+    }
+}
