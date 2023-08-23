ac993eb3c9e4957e46b32035ef07db449759c02e
denisyordanp <denis.yordan.p@gmail.com>
2023-08-23 08:11:19 +0700

    fix bluetooth connections

--- app/src/main/java/com/socialite/solite_pos/view/screens/bluetooth/BluetoothDevicesViewModel.kt
+++ app/src/main/java/com/socialite/solite_pos/view/screens/bluetooth/BluetoothDevicesViewModel.kt
@@ -8,6 +8,7 @@ import com.socialite.solite_pos.data.preference.SettingPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,16 +17,46 @@ class BluetoothDevicesViewModel @Inject constructor(
     private val getOrderWithProduct: GetOrderWithProduct,
     private val settingPreferences: SettingPreferences
 ) : ViewModel() {
-    private val _devices = MutableStateFlow(listOf<BluetoothDevice>())
-    val bluetoothDevices = _devices.asStateFlow()
+    private val _viewState = MutableStateFlow(BluetoothViewState.idle())
+    val viewState = _viewState.asStateFlow()
 
     fun setDevices(devices: List<BluetoothDevice>) = viewModelScope.launch {
-        _devices.emit(devices)
+        _viewState.emit(currentState.copy(
+            devices = devices
+        ))
     }
 
-    fun getOrder(orderId: String) = getOrderWithProduct(orderId)
+    fun getOrder(orderId: String) = viewModelScope.launch {
+        getOrderWithProduct(orderId)
+            .map {
+                currentState.copy(
+                    order = it
+                )
+            }.collect(_viewState)
+    }
 
     fun setNewPrinterDevice(address: String) {
         settingPreferences.printerDevice = address
     }
+
+    fun showSnackBar() = viewModelScope.launch {
+        _viewState.emit(currentState.copy(
+            shouldSHowSnackBar = true
+        ))
+    }
+
+    fun resetSnackBar() = viewModelScope.launch {
+        _viewState.emit(currentState.copy(
+            shouldSHowSnackBar = false,
+            isTryConnecting = false
+        ))
+    }
+
+    fun reCheckBluetooth() = viewModelScope.launch {
+        _viewState.emit(currentState.copy(
+            isTryConnecting = true
+        ))
+    }
+
+    private val currentState get() = _viewState.value
 }
---
+++ app/src/main/java/com/socialite/solite_pos/view/screens/bluetooth/BluetoothViewState.kt
@@ -0,0 +1,22 @@
+package com.socialite.solite_pos.view.screens.bluetooth
+
+import android.bluetooth.BluetoothDevice
+import com.socialite.solite_pos.data.schema.helper.OrderWithProduct
+
+data class BluetoothViewState(
+    val devices: List<BluetoothDevice>,
+    val shouldSHowSnackBar: Boolean,
+    val isTryConnecting: Boolean,
+    val order: OrderWithProduct?
+) {
+
+    val isLoading = (devices.isEmpty() || order == null) && isTryConnecting
+    companion object {
+        fun idle() = BluetoothViewState(
+            devices = emptyList(),
+            shouldSHowSnackBar = false,
+            isTryConnecting = true,
+            order = null
+        )
+    }
+}
