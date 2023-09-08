64149432666319a920d2bd4d4fc2ca06d8ee5d09
denisyordanp <denis.yordan.p@gmail.com>
2023-09-08 14:28:57 +0700

    release ver 3.3.8

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,7 @@
 object Versions {
-    fun getVersionCode() = "$majorVersion$minorVersion$patchVersion".toInt()
+    fun getVersionCode() = 43
     fun getVersionName() = "$majorVersion.$minorVersion.$patchVersion"
     private const val majorVersion = 3
     private const val minorVersion = 3
-    private const val patchVersion = 7
-
-    // This is the latest version code before we do an automatically generate version code
-    private const val latestVersionCode = 42
+    private const val patchVersion = 8
 }
