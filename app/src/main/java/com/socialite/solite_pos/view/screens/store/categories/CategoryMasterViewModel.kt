870ece93a5530d52ec5c988863d2ce10eb42ca8f
denisyordanp <denis.yordan.p@gmail.com>
2023-09-23 09:53:44 +0700

    avoid staff edit on categories

--- app/src/main/java/com/socialite/solite_pos/view/screens/store/categories/CategoryMasterViewModel.kt
+++ app/src/main/java/com/socialite/solite_pos/view/screens/store/categories/CategoryMasterViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.socialite.domain.domain.AddNewCategory
 import com.socialite.domain.domain.GetCategories
+import com.socialite.domain.domain.IsUserStaff
 import com.socialite.domain.domain.UpdateCategory
 import com.socialite.domain.schema.main.Category
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,9 +16,12 @@ class CategoryMasterViewModel @Inject constructor(
     private val getCategories: GetCategories,
     private val addNewCategory: AddNewCategory,
     private val updateCategory: UpdateCategory,
+    private val isUserStaff: IsUserStaff
 ) : ViewModel() {
 
     fun getAllCategories() = getCategories.invoke(Category.Status.ALL)
+
+    fun isUserStaff() = isUserStaff.invoke()
 
     fun insertCategory(data: Category) {
         viewModelScope.launch {
