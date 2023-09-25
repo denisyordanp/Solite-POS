74414626b29dde9751946956bfe0f2c1dc8f35cb
denisyordanp <denis.yordan.p@gmail.com>
2023-09-25 21:13:53 +0700

    release ver 3.4.2

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    fun getVersionCode() = 45
+    fun getVersionCode() = 46
     fun getVersionName() = "$majorVersion.$minorVersion.$patchVersion"
     private const val majorVersion = 3
     private const val minorVersion = 4
-    private const val patchVersion = 1
+    private const val patchVersion = 2
 }
