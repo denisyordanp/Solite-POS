2a59042e32925088af937550a4abb4098c051760
denisyordanp <denis.yordan.p@gmail.com>
2023-09-29 10:27:51 +0700

    release ver 3.5.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
     // for now to define if the app should force update is to set the version code of multiple 5
-    fun getVersionCode() = 47
+    fun getVersionCode() = 48
     fun getVersionName() = "$majorVersion.$minorVersion.$patchVersion"
     private const val majorVersion = 3
     private const val minorVersion = 5
-    private const val patchVersion = 0
+    private const val patchVersion = 1
 }
